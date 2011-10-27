3e20c30a79f26b0fa1252573efc0906d692d2200
wschwan <wschwan@d075f1d3-a90b-0410-bece-c36ad8d54cc0>
2011-10-27 11:55:08 +0000

    fix #1 . Supports max depth and stop size parameters. Next up tilles support.
    git-svn-id: https://history.iais.fraunhofer.de/svn/vrgeo/waldemar-stuff/PyMipMap@8209 d075f1d3-a90b-0410-bece-c36ad8d54cc0

--- Generator.py
+++ Generator.py
@@ -1,21 +1,65 @@
 import Image
 
-def generate(input_image_filename, output_dir, output_filename, output_posfix):
-	input_img = Image.open(input_image_filename)
-	if(not input_img):
-		raise "can't load image: " + input_image_filename
-		
-	size = input_img.size
-	level = 0
+class Generator(object):
+	"""
+	A generator class.
+	That comes with required and optional parameter.
+	"""
+	def __init__(self, input_filename):
+		self.input_filename = input_filename
+		self.stop_size = (16, 16)
+		self.max_depth = None
+		self.tile_size = None
 	
-	while(size > (8,8)):
-		img = input_img.copy()
-		img.thumbnail(size, Image.ANTIALIAS)
-		img.save(output_dir + "/" + output_filename + "L" + str(level) + "." + output_posfix, output_posfix)
+	
+	def should_create_tiles(self):
+		return (not self.tile_size is None)
+	
 		
-		size = size[0] / 2, size[1] / 2
-		level = level + 1
+	def has_max_depth(self):
+		return (not self.max_depth is None)
+	
+	
+	def set_stop_size(self, size):
+		"""
+		For your convenience. Sets the two dimentional stop size 
+		to the single value provided. Also ensured int type.
+		"""
+		self.stop_size = int(size), int(size)
+	
 		
-		print size
+	def generate(self, output_dir, output_filename, output_posfix):
+		input_img = Image.open(self.input_filename)
+		if(not input_img):
+			raise "can't load image: " + self.input_filename
+			
+		# ensure correct type
+		self.stop_size = ( int(self.stop_size[0]), int(self.stop_size[1]) )
 		
-	pass
+		size = input_img.size
+		level = 0
+		
+		print "generating mip maps for " + str(self.input_filename)
+		print "into output directory " + str(output_dir)
+		print "image info: ", input_img.format, size, input_img.mode
+		print "will stop at size ", self.stop_size
+		
+		while(size >= self.stop_size):
+			if(self.has_max_depth() and level >= self.max_depth):
+				# skipp loop as soon as we mit the max depth
+				break
+			
+			print size
+			
+			img = input_img.copy()
+			img.thumbnail(size, Image.ANTIALIAS)
+			img.save(output_dir + "/" + output_filename + "L" + str(level) + "." + output_posfix, output_posfix)
+			
+			size = ( size[0] / 2, size[1] / 2 )
+			level = level + 1
+			
+		print "done"
+	
+	
+
+
--- PyMipMap.py
+++ PyMipMap.py
@@ -1,25 +1,78 @@
 import sys
 import Generator
+import os
+import getopt
 
 def usage():
 	print """
 Usage: PyMipMap [OPTIONS]
-	-i <input_image> 						REQUIRED
-	-d <output_dir> 						uses working dir as default value
-	-o <output_image_prefix>		REQUIRED
-	-f <output_postfix>					example: png jpg
+  -h --help                   Prints this help.
+
+  -i <input_image>            REQUIRED
+  -d <output_dir>             uses working dir as default value
+  -o <output_image_prefix>    REQUIRED filename base of the output image
+  -f <output_postfix>         example: png jpg default: png
 	
-	--max-depth <i> 				Stops after creating the i'th level.
-	--stop-at-size <size>		Stops if the image whould have a smaller dimention than <size>.
+  -m --max-depth <i>          Stops after creating i images.
+  -s --stop-at-size <size>    Stops if the image whould have a smaller dimention 
+                              than <size>. Default is 16.
+	
+  -t --tile-size <tile_size>  If this value is set, all generated images will be 
+                              croped into tiles of (tile_size x tile_size).
 """
 
 def main(argv):
-	Generator.generate("/Users/velrok/Desktop/chess-board-27f0cf.jpg", "/Users/velrok/Desktop/output", "chess", "png")
-	# try:                                
-	# 	opts, args = getopt.getopt(argv, "hg:d", ["help", "grammar="])
-	# except getopt.GetoptError:
-	# 	usage()
-	# 	sys.exit(2)
+	try:                                
+		opts, args = getopt.getopt(argv, "hm:s:t:i:d:o:f:", ["help", "max-depth=", "stop-at-size=", "tile-size="])
+		
+		# set default parameters
+		input_image = None
+		output_dir = os.getcwd()
+		output_image_prefix = None
+		output_postfix = "png"
+		
+		max_depth = None
+		stop_size = 16
+		tile_size = None
+		
+		for opt, arg in opts:
+			if(opt in ("-h", "--help")):
+				usage()
+				sys.exit()
+			if(opt in ("-m", "--max-depth")):
+				max_depth = int(arg)
+			if(opt in ("-s", "--stop-at-size")):
+				stop_size = arg
+			if(opt in ("-t", "--tile-size")):
+				tile_size = arg
+			if(opt in ("-i")):
+				input_image = arg
+			if(opt in ("-d")):
+				output_dir = arg
+			if(opt in ("-o")):
+				output_image_prefix = arg
+			if(opt in ("-f")):
+				output_postfix = arg
+		
+		if not input_image:
+			print 'input image required'
+			usage()
+			sys.exit(2)
+			
+		if not output_image_prefix:
+			print 'output image prefix required'
+			usage()
+			sys.exit(2)
+			
+		g = Generator.Generator(input_image)
+		g.set_stop_size(stop_size)
+		if(max_depth):
+			g.max_depth = int(max_depth)
+		g.generate( output_dir, output_image_prefix, output_postfix )
+		
+	except getopt.GetoptError:
+		usage()
+		sys.exit(2)
 
 
 if __name__ == "__main__":
